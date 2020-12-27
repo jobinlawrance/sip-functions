72e5bde35f61ce9a394d962dc24a3ef2bfa2802e
jobinlawrance <jobinlawrance@gmail.com>
2020-12-27 19:43:05 +0530

    create auth function triggers to write to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const collectionName = 'users'
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// auth trigger (user creation) 
+exports.userRegister = functions.auth.user().onCreate(user => {
+   return admin.firestore().collection(collectionName).doc(user.uid).set({
+       email: user.email,
+       name: user.displayName,
+       profilePic: user.photoURL
+   });
+});
+
+// auth trigger (user deleted)
+exports.userDelete = functions.auth.user().onDelete(user => {
+    // for background triggers you must return a promise/value
+    var doc = admin.firestore().collection(collectionName).doc(user.uid);
+    return doc.delete();
+});
